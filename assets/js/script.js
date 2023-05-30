ff618c9272630b2f2790648bdbbd9b7de1ef4254
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-30 15:33:47 +0200

    Added milestone references

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 console.log('JS LOADED!');
+
+// Milestone 0:
+// Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del 
+// markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
+
+// Milestone 1: Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
+// Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà 
+// visibile e dovremo aggiungervi titolo e testo.
+
+// Milestone 2:
+// Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la 
+// freccia verso l'alto, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente 
+// clicca la freccia verso il basso.
+
