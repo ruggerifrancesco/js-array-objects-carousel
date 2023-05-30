0551f6d4c5ea529d37581ae6348dfd79a0f13932
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-30 17:51:41 +0200

    removed milestone 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,5 @@
 console.log('JS LOADED!');
 
-// Milestone 2:
-// Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la 
-// freccia verso l'alto, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente 
-// clicca la freccia verso il basso.
-
 const images = [
     {
         image: 'img/01.webp',
